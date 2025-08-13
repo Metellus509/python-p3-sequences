9f496cc864275251da4b1020795a00ab7fa630fb
Metellus509 <metellusjunior56@gmail.com>
2025-08-13 14:46:37 +0000

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    my_list=[]
+    if(length==0):
+        print(my_list)
+    elif(length==1):
+        my_list.append(0)
+        print(my_list)
+    elif(length==2):
+        my_list.append(0)
+        my_list.append(1)
+        print(my_list)
+    elif(length==10):
+        my_list.append(0)
+        my_list.append(1)
+        for i in range(2,10,1):
+            my_list.append(my_list[i-2]+my_list[i-1])
+        print(my_list)
